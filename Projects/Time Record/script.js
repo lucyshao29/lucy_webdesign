833db535be916b451e1570f6672723fa3db85986
lucyshao29 <lshao@risd.edu>
2018-01-23 02:17:40 -0500

    Time Final

--- Projects/Time Record/script.js
+++ Projects/Time Record/script.js
@@ -1,4 +1,186 @@
 $(document).ready(function() {
+	$('.choice1').click(function() {
+		$(this).css("background","rgb(255,255,255)");
+		$('.form2').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice2').click(function() {
+		$(this).css("background","rgb(255,255,255)");
+		$('.form8').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice3').click(function() {
+		$(this).css("background","rgb(255,255,255)");
+		$('.form3').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice4').click(function() {
+		$(this).css("background","rgb(255,255,255)");
+		$('.form4').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice5').click(function() {
+		$(this).css("background","rgb(255,255,255)");
+		$('.form4').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice6').click(function() {
+		$(this).css("background","rgb(255,255,255)");
+		$('.form5').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice7').click(function() {
+		$(this).css("background","rgb(255,255,255)");$
+		('.form11').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice8').click(function() {
+		$(this).css("background","rgb(255,255,255)");
+		$('.form6').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice9').click(function() {
+		$(this).css("background","rgb(255,255,255)");$
+		('.form12').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice10').click(function() {
+		$(this).css("background","rgb(255,255,255)");$
+		('.form13').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice11').click(function() {
+		$(this).css("background","rgb(255,255,255)");$
+		('.form13').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice14').click(function() {
+		$(this).css("background","rgb(255,255,255)");
+		$('.form2').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice15').click(function() {
+		$(this).css("background","rgb(255,255,255)");
+		$('.form9').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice16').click(function() {
+		$(this).css("background","rgb(255,255,255)");
+		$('.form2').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice17').click(function() {
+		$(this).css("background","rgb(255,255,255)");$
+		('.form10').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice18').click(function() {
+		$(this).css("background","rgb(255,255,255)");
+		$('.form2').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice19').click(function() {
+		$(this).css("background","rgb(255,255,255)");
+		$('.form2').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice20').click(function() {
+		$(this).css("background","rgb(255,255,255)");$
+		('.form13').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice21').click(function() {
+		$(this).css("background","rgb(255,255,255)");$
+		('.form12').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice22').click(function() {
+		$(this).css("background","rgb(255,255,255)");$
+		('.form13').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice23').click(function() {
+		$(this).css("background","rgb(255,255,255)");$
+		('.form13').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice24').click(function() {
+		$(this).css("background","rgb(255,255,255)");$
+		('.form14').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice25').click(function() {
+		$(this).css("background","rgb(255,255,255)");$
+		('.form15').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice26').click(function() {
+		$(this).css("background","rgb(255,255,255)");$
+		('.form15').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice27').click(function() {
+		$(this).css("background","rgb(255,255,255)");$
+		('.form17').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
+	$('.choice28').click(function() {
+		$(this).css("background","rgb(255,255,255)");$
+		('.form16').css("visibility","visible");
+	});
+});
+
+$(document).ready(function() {
 	$('.circle').mouseenter(function() {
 		$('.command').show();
 		$(this).css("background","rgba(255,255,255,1");
@@ -20,7 +202,7 @@ $(document).ready(function() {
 
 $(document).ready(function() {
 	$('.8am').click(function() {
-		$('.form').show();
+		$('.form').css("visibility","visible");
 	});
 });
 
@@ -31,9 +213,7 @@ $(document).ready(function() {
 	});
 });
 
-$(document).ready(function() {
-	$("input[name=8am]:checked").change(function() {
-		$('.form1').show();
-	});
-});
+
+
+
 
