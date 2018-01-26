ddd615d53bdb2b8289ed876ce4d7dc08483854d8
lucyshao29 <lshao@risd.edu>
2018-01-26 17:50:23 -0500

    Responsive Menu

---
+++ Exercises/Responsive Menu/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$(".icon").mouseenter(function() {
+		$(".dropdown").show();
+	});
+});
+
+$(document).ready(function() {
+	$(".icon").mouseleave(function() {
+		$(".dropdown").hide();
+	});
+});
