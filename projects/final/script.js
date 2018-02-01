7ba87013c88f1759910547e07aba6e1ad2ff364d
lucyshao29 <lshao@risd.edu>
2018-02-01 16:47:13 -0500

    Final In progress

--- projects/final/script.js
+++ projects/final/script.js
@@ -1,27 +1,43 @@
-$(document).ready(function() {
-	$('.gas').mouseenter(function() {
-		$(this).animate({opacity:0});
-	});
-});
+function opentab(tabName) {
+    var i;
+    var x = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    document.getElementById(tabName).style.display = "block"; 
+}
+
 
 $(document).ready(function() {
-	$('.gas').mouseleave(function() {
-		$(this).animate({opacity:1});
+	$('.gas').mouseenter(function() {
+		$(this).toggle('puff', function() {
+			$(this).show();
+		});
 	});
 });
 
 $(document).ready(function() {
 	$('.explode').mouseenter(function() {
-		$(this).toggle('explode');
+		$(this).toggle('explode', {pieces:36}, function() {
+			$(this).show();
+		});
 	});
 });
 
+$(document).ready(function() {
+	$('.stable').mouseenter(function() {
+		$(this).toggle('pulsate', 0.5, function() {
+			$(this).show();
+		});
+	});
+});
 
+$(document).ready(function() {
+	$('.metal').mouseenter(function() {
+		$(this).css('background','#DBDBDB');
+	});
+	$('.metal').mouseleave(function() {
+		$(this).css('background','');
+	});
+});
 
- // $(".explode").click(function(){
- //               $(".target").hide( "explode", {pieces: 16 }, 2000 );
- //            });
-
- //            $("#show").click(function(){
- //               $(".target").show( "explode", {pieces: 16}, 2000 );
- //            });
