f028c2d491c12e30d1402e8cd59835c0d6f2e9a4
lucyshao29 <lshao@risd.edu>
2018-02-03 22:26:15 -0500

    Final in progress

--- projects/final/script.js
+++ projects/final/script.js
@@ -1,3 +1,4 @@
+// Tabs
 function opentab(tabName) {
     var i;
     var x = document.getElementsByClassName("tab");
@@ -7,7 +8,120 @@ function opentab(tabName) {
     document.getElementById(tabName).style.display = "block"; 
 }
 
+// toggle mascot
+$(document).ready(function(){
+	$('img').toggle('shake', {direction:'up'}, 'slow', {number:'1'});
+});
 
+// popup window for each page start
+$(document).ready(function() {
+	$('img').mouseenter(function() {
+		$(this).hide();
+		$('.click').show();
+		$("#dialog").dialog({modal: true, height: 590, width: 1005 });
+	});
+	$('img').mouseleave(function() {
+		$(this).show();
+		$('.click').hide();
+	})
+});
+// popup window for each page end
+
+// group explanation popups
+$(document).ready(function() {
+	$('.g1').mouseenter(function(){
+		$('.g1').css('background','#BCBCBC');
+		$('.paragraph1').show();
+	});
+	$('.g1').mouseleave(function() {
+		$('.g1').css('background','');
+		$('.paragraph1').hide();
+	});
+});
+
+$(document).ready(function() {
+	$('.g2').mouseenter(function(){
+		$('.g2').css('background','#BCBCBC');
+		$('.paragraph2').show();
+	});
+	$('.g2').mouseleave(function() {
+		$('.g2').css('background','');
+		$('.paragraph2').hide();
+	});
+});
+
+$(document).ready(function() {
+	$('.g3').mouseenter(function(){
+		$('.g3').css('background','#BCBCBC');
+		$('.paragraph3').show();
+	});
+	$('.g3').mouseleave(function() {
+		$('.g3').css('background','');
+		$('.paragraph3').hide();
+	});
+});
+
+$(document).ready(function() {
+	$('.g4').mouseenter(function(){
+		$('.g4').css('background','#BCBCBC');
+		$('.paragraph4').show();
+	});
+	$('.g4').mouseleave(function() {
+		$('.g4').css('background','');
+		$('.paragraph4').hide();
+	});
+});
+
+$(document).ready(function() {
+	$('.g5').mouseenter(function(){
+		$('.g5').css('background','#BCBCBC');
+		$('.paragraph5').show();
+	});
+	$('.g5').mouseleave(function() {
+		$('.g5').css('background','');
+		$('.paragraph5').hide();
+	});
+});
+
+$(document).ready(function() {
+	$('.g16').mouseenter(function(){
+		$('.g16').css('background','#BCBCBC');
+		$('.paragraph16').show();
+	});
+	$('.g16').mouseleave(function() {
+		$('.g16').css('background','');
+		$('.paragraph16').hide();
+	});
+});
+
+$(document).ready(function() {
+	$('.g17').mouseenter(function(){
+		$('.g17').css('background','#BCBCBC');
+		$('.paragraph17').show();
+	});
+	$('.g17').mouseleave(function() {
+		$('.g17').css('background','');
+		$('.paragraph17').hide();
+	});
+});
+
+$(document).ready(function() {
+	$('.g18').mouseenter(function(){
+		$('.g18').css('background','#BCBCBC');
+		$('.paragraph18').show();
+	});
+	$('.g18').mouseleave(function() {
+		$('.g18').css('background','');
+		$('.paragraph18').hide();
+	});
+});
+//group explanation popups end
+
+$(document).ready(function() {
+	$('.paragraph').hide();
+});
+
+// special effects start
 $(document).ready(function() {
 	$('.gas').mouseenter(function() {
 		$(this).toggle('puff', function() {
@@ -40,4 +154,4 @@ $(document).ready(function() {
 		$(this).css('background','');
 	});
 });
-
+// Effects end
