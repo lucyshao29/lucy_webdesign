e8642e75081b5e4003a400ba85369f594247b9b7
lucyshao29 <lshao@risd.edu>
2018-02-05 13:17:56 -0500

    Final in progress

--- projects/final/script.js
+++ projects/final/script.js
@@ -10,23 +10,129 @@ function opentab(tabName) {
 
 // toggle mascot
 $(document).ready(function(){
-	$('img').toggle('shake', {direction:'up'}, 'slow', {number:'1'});
+	$('.flask').toggle('shake', {direction:'up', times: 3, distance:10}, 'slow', function() {
+		$(this).show();
+	});
+});
+
+$(document).ready(function(){
+	$('#flaskIon').toggle('shake', {direction:'up', times: 3, distance:10}, 'slow', function() {
+		$(this).show();
+	});
 });
 
 // popup window for each page start
 $(document).ready(function() {
-	$('img').mouseenter(function() {
-		$(this).hide();
+	$('.flask').mouseenter(function() {
+		$('.flask').hide();
 		$('.click').show();
 		$("#dialog").dialog({modal: true, height: 590, width: 1005 });
 	});
-	$('img').mouseleave(function() {
-		$(this).show();
+	$('.click').mouseleave(function() {
+		$('.flask').show();
 		$('.click').hide();
 	})
 });
 // popup window for each page end
 
+// Ion page
+$(document).ready(function() {
+	$('h1').mouseenter(function() {
+		$(this).next('h4').show();
+	});
+	$('h1').mouseleave(function() {
+		$(this).next('h4').hide();
+	});
+});
+
+$(document).ready(function() {
+	$('.colorless').mouseenter(function() {
+		$(this).css('background','rgba(0,0,0,0)');
+		$(this).css('box-shadow','3px 3px rgba(0,0,0,0)');
+	});
+	$('.colorless').mouseleave(function() {
+		$(this).css('background','');
+		$(this).css('box-shadow','');
+	});
+});
+
+$(document).ready(function() {
+	$('.purple').mouseenter(function() {
+		$(this).css('background','rgba(121,0,213,1)');
+		$(this).css('box-shadow','3px 3px rgb(92,0,162)');
+	});
+	$('.purple').mouseleave(function() {
+		$(this).css('background','');
+		$(this).css('box-shadow','');
+	});
+});
+
+$(document).ready(function() {
+	$('.palepink').mouseenter(function() {
+		$(this).css('background','rgb(255,213,247)');
+		$(this).css('box-shadow','3px 3px rgb(255,196,244)');
+	});
+	$('.palepink').mouseleave(function() {
+		$(this).css('background','');
+		$(this).css('box-shadow','');
+	});
+});
+
+$(document).ready(function() {
+	$('.pink').mouseenter(function() {
+		$(this).css('background','rgb(255,104,241)');
+		$(this).css('box-shadow','3px 3px rgb(230,69,214)');
+	});
+	$('.pink').mouseleave(function() {
+		$(this).css('background','');
+		$(this).css('box-shadow','');
+	});
+});
+
+$(document).ready(function() {
+	$('.green').mouseenter(function() {
+		$(this).css('background','rgb(30,223,0)');
+		$(this).css('box-shadow','3px 3px rgb(25,191,0)');
+	});
+	$('.green').mouseleave(function() {
+		$(this).css('background','');
+		$(this).css('box-shadow','');
+	});
+});
+
+$(document).ready(function() {
+	$('.bluepurple').mouseenter(function() {
+		$(this).removeClass('element');
+		$(this).addClass('bluepurplebp');
+	});
+	$('.bluepurple').mouseleave(function() {
+		$(this).removeClass('bluepurplebp');
+		$(this).addClass('element');
+	});
+});
+
+$(document).ready(function() {
+	$('.colorlessblue').mouseenter(function() {
+		$(this).removeClass('element');
+		$(this).addClass('colorlessbluecb');
+	});
+	$('.colorlessblue').mouseleave(function() {
+		$(this).removeClass('colorlessbluecb');
+		$(this).addClass('element');
+	});
+});
+
+$(document).ready(function() {
+	$('.greenyellow').mouseenter(function() {
+		$(this).removeClass('element');
+		$(this).addClass('greenyellowgy');
+	});
+	$('.greenyellow').mouseleave(function() {
+		$(this).removeClass('greenyellowgy');
+		$(this).addClass('element');
+	});
+});
+// Ion Page End
 // group explanation popups
 $(document).ready(function() {
 	$('.g1').mouseenter(function(){
@@ -131,27 +237,101 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-	$('.explode').mouseenter(function() {
-		$(this).toggle('explode', {pieces:36}, function() {
-			$(this).show();
-		});
+	$('#liquid').mouseenter(function() {
+		$(this).css('animation-play-state','running');
+	});
+	$('#liquid').mouseleave(function() {
+		$(this).css('animation-play-state','paused');
 	});
 });
 
 $(document).ready(function() {
-	$('.stable').mouseenter(function() {
-		$(this).toggle('pulsate', 0.5, function() {
+	$('#liquid1').mouseenter(function() {
+		$(this).css('animation-play-state','running');
+	});
+	$('#liquid1').mouseleave(function() {
+		$(this).css('animation-play-state','paused');
+	});
+});
+
+
+$(document).ready(function(){
+	$('.solid').mouseenter(function() {
+		$(this).toggle('shake', {direction:'left', times: 3, distance:3}, function() {
 			$(this).show();
 		});
 	});
 });
+// Special effects end
 
+// Orbital start
 $(document).ready(function() {
-	$('.metal').mouseenter(function() {
-		$(this).css('background','#DBDBDB');
+	$('.os').mouseenter(function(){
+		$('.os').css('background','#BCBCBC');
+		$('.paragraphS').show();
 	});
-	$('.metal').mouseleave(function() {
-		$(this).css('background','');
+	$('.os').mouseleave(function() {
+		$('.os').css('background','');
+		$('.paragraphS').hide();
 	});
 });
-// Effects end
+
+$(document).ready(function() {
+	$('.p').mouseenter(function(){
+		$('.p').css('background','#BCBCBC');
+		$('.paragraphp').show();
+	});
+	$('.p').mouseleave(function() {
+		$('.p').css('background','');
+		$('.paragraphp').hide();
+	});
+});
+
+$(document).ready(function() {
+	$('.d').mouseenter(function(){
+		$('.d').css('background','#BCBCBC');
+		$('.paragraphd').show();
+	});
+	$('.d').mouseleave(function() {
+		$('.d').css('background','');
+		$('.paragraphd').hide();
+	});
+});
+
+$(document).ready(function() {
+	$('.f').mouseenter(function(){
+		$('.f').css('background','#BCBCBC');
+		$('.paragraphf').show();
+	});
+	$('.f').mouseleave(function() {
+		$('.f').css('background','');
+		$('.paragraphf').hide();
+	});
+});
+
+
+// $(document).ready(function() {
+// 	$('.explode').mouseenter(function() {
+// 		$(this).toggle('explode', {pieces:36}, function() {
+// 			$(this).show();
+// 		});
+// 	});
+// });
+
+// $(document).ready(function() {
+// 	$('.stable').mouseenter(function() {
+// 		$(this).toggle('pulsate', 0.5, function() {
+// 			$(this).show();
+// 		});
+// 	});
+// });
+
+// $(document).ready(function() {
+// 	$('.metal').mouseenter(function() {
+// 		$(this).css('background','#DBDBDB');
+// 	});
+// 	$('.metal').mouseleave(function() {
+// 		$(this).css('background','');
+// 	});
+// });
+// // Effects end
